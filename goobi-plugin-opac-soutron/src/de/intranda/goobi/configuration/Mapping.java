7bf29da12a160fa1182fdf5b8d3fc880b0ac3aa1
Steffen Hankiewicz <steffen.hankiewicz@intranda.com>
2018-02-12 14:57:35 +0300

    updated configuration reading

--- goobi-plugin-opac-soutron/src/de/intranda/goobi/configuration/Mapping.java
+++ goobi-plugin-opac-soutron/src/de/intranda/goobi/configuration/Mapping.java
@@ -6,83 +6,79 @@ import java.util.Map;
 
 import org.apache.commons.configuration.ConfigurationException;
 import org.apache.commons.configuration.HierarchicalConfiguration;
+import org.apache.commons.configuration.SubnodeConfiguration;
 import org.apache.commons.configuration.XMLConfiguration;
 import org.apache.commons.configuration.reloading.FileChangedReloadingStrategy;
 import org.apache.commons.configuration.tree.xpath.XPathExpressionEngine;
 
+import de.sub.goobi.config.ConfigPlugins;
+import de.sub.goobi.helper.Helper;
 import lombok.extern.log4j.Log4j;
 
 @Log4j
 public class Mapping {
 
-    private static final String configurationFile = "/opt/digiverso/goobi/config/plugin_opac-soutron.xml";
+	private static Mapping instance;
+	private static XMLConfiguration config;
 
-    private static Mapping instance;
-    private static XMLConfiguration config;
+	public static final String FIRSTNAME = "firstname";
+	public static final String LASTNAME = "lastname";
+	public static final String METADATA = "metadata";
+	public static final String IDENTIFIER = "identifier";
 
-    public static final String FIRSTNAME = "firstname";
-    public static final String LASTNAME = "lastname";
-    public static final String METADATA = "metadata";
-    public static final String IDENTIFIER = "identifier";
+	public static synchronized Mapping getInstance() {
+		if (instance == null) {
+			instance = new Mapping();
+		}
+		return instance;
+	}
 
-    public static synchronized Mapping getInstance() {
-        if (instance == null) {
-            instance = new Mapping();
-        }
-        return instance;
-    }
+	private Mapping() {
+		String configFile = "plugin_SoutronImport.xml";
+		try {
+			config = new XMLConfiguration(new Helper().getGoobiConfigDirectory() + configFile);
+		} catch (ConfigurationException e) {
+			log.error(e);
+			config = new XMLConfiguration();
+		}
+		config.setReloadingStrategy(new FileChangedReloadingStrategy());
+		config.setExpressionEngine(new XPathExpressionEngine());
+	}
 
-    private Mapping() {
-        config = getConfiguration();
-    }
+	public Map<String, String> getConfiguredDocstructs() {
+		Map<String, String> docstructMap = new HashMap<>();
 
-    private static XMLConfiguration getConfiguration() {
-        XMLConfiguration config;
-        try {
-            config = new XMLConfiguration(configurationFile);
-        } catch (ConfigurationException e) {
-            log.error(e);
-            config = new XMLConfiguration();
-        }
-        config.setReloadingStrategy(new FileChangedReloadingStrategy());
-        config.setExpressionEngine(new XPathExpressionEngine());
-        return config;
-    }
+		List<HierarchicalConfiguration> docstructList = config.configurationsAt("docstructs/docstruct");
+		for (HierarchicalConfiguration docstruct : docstructList) {
+			String actaproName = docstruct.getString("@soutron");
+			String rulesetName = docstruct.getString("@ruleset");
+			docstructMap.put(actaproName, rulesetName);
+		}
+		return docstructMap;
+	}
 
-    public Map<String, String> getConfiguredDocstructs() {
-        Map<String, String> docstructMap = new HashMap<>();
+	public Map<String, String> getConfiguredMetadataMapping() {
+		Map<String, String> metadataMap = new HashMap<>();
 
-        List<HierarchicalConfiguration> docstructList = config.configurationsAt("docstructs/docstruct");
-        for (HierarchicalConfiguration docstruct : docstructList) {
-            String actaproName = docstruct.getString("@soutron");
-            String rulesetName = docstruct.getString("@ruleset");
-            docstructMap.put(actaproName, rulesetName);
-        }
-        return docstructMap;
-    }
+		List<HierarchicalConfiguration> docstructList = config.configurationsAt("metadata/element");
+		for (HierarchicalConfiguration docstruct : docstructList) {
+			String xpath = docstruct.getString("@xpath");
+			String metadataName = docstruct.getString("@metadata");
+			metadataMap.put(xpath, metadataName);
+		}
+		return metadataMap;
+	}
 
-    public Map<String, String> getConfiguredMetadataMapping() {
-        Map<String, String> metadataMap = new HashMap<>();
+	public Map<String, String> getConfiguredPersonMapping() {
+		Map<String, String> allPersons = new HashMap<>();
 
-        List<HierarchicalConfiguration> docstructList = config.configurationsAt("metadata/element");
-        for (HierarchicalConfiguration docstruct : docstructList) {
-            String xpath = docstruct.getString("@xpath");
-            String metadataName = docstruct.getString("@metadata");
-            metadataMap.put(xpath, metadataName);
-        }
-        return metadataMap;
-    }
-
-    public Map<String, String> getConfiguredPersonMapping() {
-        Map<String, String> allPersons = new HashMap<>();
-
-        List<HierarchicalConfiguration> personElementList = config.configurationsAt("person/element");
-        for (HierarchicalConfiguration personElement : personElementList) {
-            String xpath = personElement.getString("@xpath");
-            String metadataName = personElement.getString("@metadata");
-            allPersons.put(xpath, metadataName);
-        }
-        return allPersons;
-    }
+		List<HierarchicalConfiguration> personElementList = config.configurationsAt("person/element");
+		for (HierarchicalConfiguration personElement : personElementList) {
+			String xpath = personElement.getString("@xpath");
+			String metadataName = personElement.getString("@metadata");
+			allPersons.put(xpath, metadataName);
+		}
+		return allPersons;
+	}
 
 }
