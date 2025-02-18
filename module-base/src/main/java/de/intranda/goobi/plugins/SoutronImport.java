60ea23aa915bb6b9fa8454c09b2d247a41fa8052
Dominick Leppich <dominick.leppich@intranda.com>
2025-02-18 12:03:05 +0100

    fix: Tomcat 10 namespace fixes

--- module-base/src/main/java/de/intranda/goobi/plugins/SoutronImport.java
+++ module-base/src/main/java/de/intranda/goobi/plugins/SoutronImport.java
@@ -29,10 +29,10 @@ import java.io.StringReader;
 import java.util.List;
 import java.util.Map;
 
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.WebTarget;
-import javax.ws.rs.core.Response;
+import jakarta.ws.rs.client.Client;
+import jakarta.ws.rs.client.ClientBuilder;
+import jakarta.ws.rs.client.WebTarget;
+import jakarta.ws.rs.core.Response;
 
 import org.goobi.production.enums.PluginType;
 import org.goobi.production.plugin.interfaces.IOpacPlugin;
